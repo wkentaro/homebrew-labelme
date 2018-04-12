19f178f03b00f1e1f7a0d2b60909bff22a4afc2f
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-04-13 00:24:27 +0900

    labelme: 2.11.0

--- Casks/labelme.rb
+++ Casks/labelme.rb
@@ -1,8 +1,8 @@
 cask 'labelme' do
-  version '2.10.0'
+  version '2.11.0'
   sha256 'f5c6b4d90a1e4132027060598696df00cf867f06610f14faa66de64a90de7a0a'
 
-  url 'https://github.com/wkentaro/labelme/releases/download/v2.10.0/labelme-macOS.app.zip'
+  url 'https://github.com/wkentaro/labelme/releases/download/v2.11.0/labelme-macOS.app.zip'
   name 'labelme'
   homepage 'https://github.com/wkentaro/labelme'
 
--- labelme.rb
+++ labelme.rb
@@ -1,8 +1,8 @@
 class Labelme < Formula
   desc "Image Polygonal Annotation with Python."
   homepage "https://github.com/wkentaro/labelme"
-  url "https://github.com/wkentaro/labelme/releases/download/v2.10.0/labelme-macOS.zip"
-  version "2.10.0"
+  url "https://github.com/wkentaro/labelme/releases/download/v2.11.0/labelme-macOS.zip"
+  version "2.11.0"
   sha256 "8f221500a75a938dd33f3488af46f7152aeafb4ae4c21687e2d6908d5884dace"
 
   def install
